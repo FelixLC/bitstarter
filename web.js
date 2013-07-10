526f414da43fe3134c7cc8a3de7aab68952ab207
FelixLC <felix.lechevallier@gmail.com>
2013-07-10 17:07:27 +0000

    Modified 3 web.js

--- web.js
+++ web.js
@@ -5,7 +5,6 @@ app.get('/', function(request, response) {
 var fs = require('fs');
 var buffer = new buffer ();
   response.send(buffer.toString('utf8', fs.readFileSync("index.html")))});
- });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
