9bb8655fad429bcc00395a7f5d423cc66fcff8b5
FelixLC <felix.lechevallier@gmail.com>
2013-07-10 17:10:22 +0000

    Modified 3 web.js

--- web.js
+++ web.js
@@ -3,8 +3,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 var fs = require('fs');
-var buffer = new buffer ();
-  response.send(buffer.toString('utf8', fs.readFileSync("index.html")))});
+  response.send(fs.readFileSync("index.html", 'utf8')))};
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
